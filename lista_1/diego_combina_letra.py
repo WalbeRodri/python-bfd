9d3647070e6605f6a397d21394147f9ae38c1617
DiegoL13 <diegolps1317@gmail.com>
2025-08-27 17:54:39 -0300

    combinacoes de letras do alfabeto

---
+++ lista_1/diego_combina_letra.py
@@ -0,0 +1,6 @@
+alfabeto=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for x in alfabeto:
+    for y in alfabeto:
+        print(x,y)
