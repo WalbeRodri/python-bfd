a093f26dead1ecaa51f64ed9bbe141509f3f3f77
ThiagoEndrigo <116767258+ThiagoEndrigo@users.noreply.github.com>
2025-08-27 23:38:54 -0300

    Envio das atividades

--- lista_1/Endrigo_contagem_01.py
+++ lista_1/Endrigo_contagem_01.py
@@ -2,7 +2,7 @@
 # Prof.: Walber Olibeira
 # Turma: 11.
 
-# Escreva um programa que imprima os números de 1 a 10 usando um loop for
+# Escreva um programa que imprima os números de 1 a 10 usando um loop for.
 
 
 num = 0
